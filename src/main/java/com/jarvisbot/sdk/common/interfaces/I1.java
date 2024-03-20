7cfa3ecf893567d579720e4c434c1c18c952bf8c
Guo Changzhu <guochangzhu@qq.com>
2024-03-20 09:18:53 +0800

    update copyright

--- src/main/java/com/jarvisbot/sdk/common/interfaces/I1.java
+++ src/main/java/com/jarvisbot/sdk/common/interfaces/I1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 FanTe.Company
+ * Copyright 2024 JarvisBot.ai. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
